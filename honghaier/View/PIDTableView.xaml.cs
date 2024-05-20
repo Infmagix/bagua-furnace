3db03e16c1b47a059fbcdff7752bebe896f45ec4
infmagix <infmagix.jason@gmail.com>
2024-05-20 17:12:01 +0800

    refactor(PIDTable view): Refactor codes to support testing of PID parameters tuning

--- honghaier/View/PIDTableView.xaml.cs
+++ honghaier/View/PIDTableView.xaml.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using honghaier.Utility;
+using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace honghaier.View
 {
@@ -12,8 +12,12 @@ namespace honghaier.View
         private int nVisibleZones = 1;
 
         // test
-        private int nPIDSize = 1; // 各区域的温度段数
-        private List<PID_Parameter> pidParamList;
+        private int nZones = 4;
+        private int nPIDs = 4; // 各区域的温度段数
+        private Dictionary<string, List<PID_Parameter>> zoneID2PidParamList;
+
+        private log4net.ILog log = log4net.LogManager.GetLogger("PIDTableView");
+
 
         public PIDTableView()
         {
@@ -31,52 +35,143 @@ namespace honghaier.View
             GenPIDData();
 
 
-            //InitUI();
+            InitUI();
         }
+
+        //private void InitUI(int ii)
+        //{
+        //    PIDTabControl.Items.Clear();
+
+        //    TabItem zoneTabItem;
+        //    StackPanel zoneStackPanel;
+
+        //    for (int i = 0; i < nVisibleZones; i++)
+        //    {
+        //        zoneTabItem = new TabItem()
+        //        {
+        //            Header = $"Zone{i}",
+        //            Name = $"PIDTable_Zone{i}TabItem",
+        //        };
+
+        //        zoneStackPanel = new StackPanel()
+        //        { 
+        //            Orientation = Orientation.Horizontal,
+        //            Name = $"PIDTable_Zone{i}StackPanel",
+        //        };
+        //        // 添加控件到Zone{i}的StackPanel中
+        //        for (int j = 0; j < pidParamList.Count; j++)
+        //        {
+        //            zoneStackPanel.Children.Add(CreateVerticalStack("Index ", $"{pidParamList[j].Index}"));
+        //            zoneStackPanel.Children.Add(CreateVerticalStack("  Kp  ", $"{pidParamList[j].Kp}"));
+        //            zoneStackPanel.Children.Add(CreateVerticalStack("  Ki  ", $"{pidParamList[j].Ki}"));
+        //            zoneStackPanel.Children.Add(CreateVerticalStack("  Kd  ", $"{pidParamList[j].Kd}"));
+        //            zoneStackPanel.Children.Add(CreateVerticalStack("MAXOUT", $"{pidParamList[j].MaxOut}"));
+        //        }
+
+        //        zoneTabItem.Content = zoneStackPanel;
+        //        PIDTabControl.Items.Add(zoneTabItem);
+        //    }
+        //}
 
         private void InitUI()
         {
-            PIDTabControl.Items.Clear();
-
-            TabItem zoneTabItem;
-            StackPanel zoneStackPanel;
-
-            for (int i = 0; i < nVisibleZones; i++)
+            for (int i = 0; i < zoneID2PidParamList.Count; i++)
             {
-                zoneTabItem = new TabItem()
+                if (PIDTabControl.Items[i] is TabItem tabItem)
                 {
-                    Header = $"Zone{i}",
-                    Name = $"PIDTable_Zone{i}TabItem",
-                };
-
-                zoneStackPanel = new StackPanel()
-                { 
-                    Orientation = Orientation.Horizontal,
-                    Name = $"PIDTable_Zone{i}StackPanel",
-                };
-                // 添加控件到Zone{i}的StackPanel中
-                for (int j = 0; j < pidParamList.Count; j++)
-                {
-                    zoneStackPanel.Children.Add(CreateVerticalStack("Index ", $"{pidParamList[j].Index}"));
-                    zoneStackPanel.Children.Add(CreateVerticalStack("  Kp  ", $"{pidParamList[j].Kp}"));
-                    zoneStackPanel.Children.Add(CreateVerticalStack("  Ki  ", $"{pidParamList[j].Ki}"));
-                    zoneStackPanel.Children.Add(CreateVerticalStack("  Kd  ", $"{pidParamList[j].Kd}"));
-                    zoneStackPanel.Children.Add(CreateVerticalStack("MAXOUT", $"{pidParamList[j].MaxOut}"));
+                    if (tabItem.Content is StackPanel vsp)
+                    {
+                        var key = $"Zone{i}";
+                        var pidParamList = zoneID2PidParamList[key];
+                        for (int j = 0; j < pidParamList.Count; j++)
+                        {
+                            var hsp = CreateHorizontalStackPanel(key + $"_{j}_", pidParamList[j]);
+                            vsp.Children.Add(hsp);
+                        }
+                    }
                 }
-
-                zoneTabItem.Content = zoneStackPanel;
-                PIDTabControl.Items.Add(zoneTabItem);
             }
         }
 
-        // Create count zone TabItems in PID TabControl
-        private void CreateZoneItems(int count)
+        private StackPanel CreateHorizontalStackPanel(string id, PID_Parameter pidParam)
         {
+            var sp = new StackPanel() { 
+                Orientation = Orientation.Horizontal,
+            };
+            var paramArr = pidParam.GetAllValues();
 
+            TextBox tb;
+            for (int i = 0; i < paramArr.Length; i++)
+            {
+                tb = CreateTextBox(id + pidParam.GetPropertyName(i), $"{paramArr[i]}");
+                sp.Children.Add(tb);
+            }
+            
+            //TextBox indexTB = CreateTextBox($"{pidParam.Index}");
+            //sp.Children.Add(indexTB);
+
+            //TextBox lowerTB = CreateTextBox($"{pidParam.LowerLimit}");
+            //sp.Children.Add(lowerTB);
+
+            //TextBox upperTB = CreateTextBox($"{pidParam.UpperLimit}");
+            //sp.Children.Add(upperTB);
+
+            //TextBox kpTB = CreateTextBox($"{pidParam.Kp}");
+            //sp.Children.Add(kpTB);
+
+            //TextBox kiTB = CreateTextBox($"{pidParam.Ki}");
+            //sp.Children.Add(kiTB);
+
+            //TextBox kdTB = CreateTextBox($"{pidParam.Kd}");
+            //sp.Children.Add(kdTB);
+
+            //TextBox maxoutTB = CreateTextBox($"{pidParam.MaxOut}");
+            //sp.Children.Add(maxoutTB);
+
+            return sp;
         }
+        
+        private TextBox CreateTextBox(string name, string text)
+        {
+            var tb = new TextBox() {
+                Text = text ?? "",
+                HorizontalContentAlignment = HorizontalAlignment.Center, 
+                Width = 70,
+                Name = name,
+            };
+            tb.TextChanged += (sender, e) => { 
+                var tBox = sender as TextBox;
+                if (!string.IsNullOrEmpty(tBox.Text))
+                {
+                    var strs = tBox.Name.Split('_');
+                    int idx = int.Parse(strs[1]);
+                    var ref2PIDParam = zoneID2PidParamList[strs[0]][idx];
 
+                    if ("Index" == strs[2])
+                        ref2PIDParam.Index = int.Parse(tBox.Text);
+                    else if ("LowerLimit" == strs[2])
+                        ref2PIDParam.LowerLimit = double.Parse(tBox.Text);
+                    else if ("UpperLimit" == strs[2])
+                        ref2PIDParam.UpperLimit = double.Parse(tBox.Text);
+                    else if ("Kp" == strs[2])
+                        ref2PIDParam.Kp = double.Parse(tBox.Text);
+                    else if ("Ki" == strs[2])
+                        ref2PIDParam.Ki = double.Parse(tBox.Text);
+                    else if ("Kd" == strs[2])
+                        ref2PIDParam.Kd = double.Parse(tBox.Text);
+                    else if ("MaxOut" == strs[2])
+                        ref2PIDParam.MaxOut = double.Parse(tBox.Text);
+                    else
+                    {
+                        // error
+                    }
 
-
+                    LogHelper.Debug(strs[0] + " changed:", log);
+                    LogHelper.Debug(ref2PIDParam.ToString(), log);
+                }
+            };
+            return tb;
+        }
 
 
 
@@ -97,10 +192,21 @@ namespace honghaier.View
         {
             try
             {
-                pidParamList = new List<PID_Parameter>();
-                for (int i = 0; i < nPIDSize; i++)
+                zoneID2PidParamList = new Dictionary<string, List<PID_Parameter>>();
+                for (int i = 0; i < nZones; i++)
                 {
-                    pidParamList.Add(new PID_Parameter(i, i * 100, i * 100 + 100, i + 1, i + 2, i + 3, 10000));
+                    LogHelper.Debug($"Initializing Zone{i}:", log);
+
+                    var pidParamList = new List<PID_Parameter>();
+                    pidParamList.Add(new PID_Parameter(0, 0, 100, 1, 2, 3, 10000));
+                    LogHelper.Debug(pidParamList[0].ToString(), log);
+
+                    for (int j = 0; j < nPIDs; j++)
+                    {
+                        pidParamList.Add(new PID_Parameter(j, j * 100 + 100, j * 100 + 200, i + 1, i + 2, i + 3, 10000));
+                        LogHelper.Debug(pidParamList[j + 1].ToString(), log);
+                    }
+                    zoneID2PidParamList[$"Zone{i}"] = pidParamList;
                 }
 
             }
@@ -139,5 +245,21 @@ namespace honghaier.View
         public double Ki { get => ki; set => ki = value; }
         public double Kd { get => kd; set => kd = value; }
         public double MaxOut { get => maxOut; set => maxOut = value; }
+
+        public double[] GetAllValues()
+        {
+            return new double[] { Index, LowerLimit, UpperLimit, Kp, Ki, Kd, MaxOut };
+        }
+
+        public string GetPropertyName(int index)
+        {
+            var strs = new string[] { "Index", "LowerLimit", "UpperLimit", "Kp", "Ki", "Kd", "MaxOut", };
+            return strs[index];
+        }
+
+        override public string ToString()
+        {
+            return $"Index={Index} LL={LowerLimit} UL={UpperLimit} Kp={Kp} Ki={Ki} Kd={Kd} MaxOut={MaxOut}";
+        }
     }
 }
